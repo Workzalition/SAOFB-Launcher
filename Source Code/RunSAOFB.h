91b2b742b7b660624713c4401c5748d0d27de345
Tee Yuan <yuantee1990@outlook.com>
2024-02-13 17:12:43 +0800

    Change the startup directory to relative directory.

--- Source Code/RunSAOFB.h
+++ Source Code/RunSAOFB.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Windows.h>
 #include <iostream>
+
 VOID startup(LPWSTR lpApplicationName)
 {
 	// additional information
@@ -12,23 +13,24 @@ VOID startup(LPWSTR lpApplicationName)
 	ZeroMemory(&pi, sizeof(pi));
 
 	// start the program up
-	CreateProcessW(lpApplicationName,   // the path
-		0,        // Command line
-		NULL,           // Process handle not inheritable
-		NULL,           // Thread handle not inheritable
-		FALSE,          // Set handle inheritance to FALSE
-		0,              // No creation flags
-		NULL,           // Use parent's environment block
-		NULL,           // Use parent's starting directory 
-		&si,            // Pointer to STARTUPINFO structure
-		&pi             // Pointer to PROCESS_INFORMATION structure (removed extra parentheses)
+	CreateProcessW(lpApplicationName, // the path
+				   0,				  // Command line
+				   NULL,			  // Process handle not inheritable
+				   NULL,			  // Thread handle not inheritable
+				   FALSE,			  // Set handle inheritance to FALSE
+				   0,				  // No creation flags
+				   NULL,			  // Use parent's environment block
+				   NULL,			  // Use parent's starting directory
+				   &si,				  // Pointer to STARTUPINFO structure
+				   &pi				  // Pointer to PROCESS_INFORMATION structure (removed extra parentheses)
 	);
-	// Close process and thread handles. 
+	// Close process and thread handles.
 	CloseHandle(pi.hProcess);
 	CloseHandle(pi.hThread);
 }
-bool RunSAOFB() 
+
+bool RunSAOFB()
 {
-	startup((LPWSTR)L"R:\\Games\\Sword Art Online Fatal Bullet Dissonance of the Nexus\\SAOFB\\Binaries\\Win64\\SAOFB-Win64-Shipping.exe"); //Sword Art Online Fatal Bullet Auto Launcher
+	startup((LPWSTR)L".\\SAOFB\\Binaries\\Win64\\SAOFB-Win64-Shipping.exe"); // Sword Art Online Fatal Bullet Auto Launcher
 	return GetLastError();
 }
